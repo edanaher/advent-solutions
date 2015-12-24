e3526c6ba274c2c5f561268fbe08def320530d92
Evan Danaher <github@edanaher.net>
2015-12-24 11:04:02 -0500

    Day 24 ruby solutions.  15 minutes flat for 15th place, though part b is wrong; it doesn't check that the third and fourth groups can be balanced.  Oddly, even checking if group 2 can be the right size is unnecessary on my input.  Probably could have saved a bit if I hadn't bothered with it (though copy/paste meant it was fairly quick).

---
+++ 24/sol.24b.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+packages = File.readlines("input.24").map(&:to_i)
+
+puts packages.inspect
+$maxweight = packages.max
+$targetweight = packages.inject {|a,b| a + b} / 4
+puts $targetweight
+
+arr = [packages.size + 2] * ($targetweight + $maxweight + 5)
+arr[0] = 0
+packages.each do |p|
+  $targetweight.downto(0) do |w|
+    arr[w + p] = [arr[w+p], arr[w] + 1].min
+  end
+end
+
+puts arr.inspect
+$group1size = arr[$targetweight]
+puts $group1size
+$packages = packages
+
+def buildpackages(sofar, sum, w)
+  if sofar.size == $group1size
+    return unless sum == $targetweight
+
+    packages = $packages - sofar
+    arr = [packages.size + 2] * ($targetweight + $maxweight + 5)
+    arr[0] = 0
+    packages.each do |p|
+      $targetweight.downto(0) do |w|
+        arr[w + p] = [arr[w+p], arr[w] + 1].min
+      end
+    end
+    #puts arr[$targetweight]
+    return if arr[$targetweight] > packages.size
+
+    quantum = sofar.inject {|a,b| a * b}
+    puts "#{quantum}: #{sofar.inspect}"
+  end
+  
+  (w..$packages.size-1).each do |p|
+    sofar << $packages[p]
+    buildpackages(sofar, sum + $packages[p], p+1)
+    sofar.pop
+  end
+end
+
+buildpackages([], 0, 0)
