1ee01af56921c31a2b0eddc6ca09965472d6da38
Evan Danaher <github@edanaher.net>
2016-01-17 15:16:02 -0500

    Day 3 nix solutions.  I'm getting the hang of it.

---
+++ 3/sol.3a.nix
@@ -0,0 +1,20 @@
+let strings = import <nixpkgs/lib/strings.nix>;
+    update_position = pos: c:
+      let x = builtins.head pos;
+          y = builtins.elemAt pos 1;
+      in
+      if c == "<" then [(x - 1) y] else
+      if c == ">" then [(x + 1) y] else
+      if c == "v" then [x (y + 1)] else
+      if c == "^" then [x (y - 1)] else
+      [x y];
+    input = strings.stringToCharacters (builtins.readFile ./input.3);
+    runPath = path: pos: soFar:
+      let pos' = update_position pos (builtins.head path);
+          soFar' = if builtins.elem pos soFar then soFar else soFar ++ [pos];
+      in
+      if path == [] then soFar' else
+      runPath (builtins.tail path) pos' soFar';
+    coords = runPath input [0 0] [];
+in
+builtins.length coords
